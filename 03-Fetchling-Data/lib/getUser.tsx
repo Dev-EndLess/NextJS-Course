c1608239a63207a5398e9de4dc7a3e9195e444c2
Dev-EndLess <endlesspain1988@gmail.com>
2023-06-13 09:37:12 +0200

    removed wrong directory

--- 03-Fetchling-Data/lib/getUser.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getUser( userId: string ) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-
-  if (!response.ok) {
-    console.log('Data Fetch Failed...')
-  }
-
-  return response.json()
-}
